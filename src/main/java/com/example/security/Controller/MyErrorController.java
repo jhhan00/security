7f3523fa671d8829585a13da6ce85d6545335b75
Lenovo <jhhan009@gmail.com>
2020-03-03 17:32:05 +0900

    second commit

---
+++ src/main/java/com/example/security/Controller/MyErrorController.java
@@ -0,0 +1,37 @@
+package com.example.security.Controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class MyErrorController implements ErrorController {
+
+    private static final String ERROR_PATH = "/error";
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
+    @RequestMapping("/error")
+    public String ErrorHandle(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        HttpStatus httpStatus = HttpStatus.valueOf(Integer.valueOf(status.toString()));
+        log.info("httpStatus : " + httpStatus.toString());
+        model.addAttribute("code",status.toString());
+        model.addAttribute("msg",httpStatus.getReasonPhrase());
+        model.addAttribute("timeStamp", new Date());
+
+        return "error";
+    }
+}
